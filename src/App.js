c0280647e755c75eb657025d9415abb1f06c9c77
CARLOS VIERA <pisis_13_3@hotmail.com>
2022-01-16 14:31:19 -0500

    Agregando f_contarCadena, App.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,28 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0
+    }
+    // Este enlace es necesario para hacer que `this` funcione en el callback
+    this.contarcadena = this.contarcadena.bind(this); 
+  }
+  contarcadena(event) {
+    this.setState({
+      count:  event.target.value.length
+    })
+  }
   render() {
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+        <textarea rows="3" onKeyPressCapture={this.contarcadena}></textarea>
+        <div className="counter">{this.state.count}</div>
       </div>
     );
   }
+
 }
 
 export default App;
